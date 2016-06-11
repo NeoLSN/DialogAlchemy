2ce3d84f20a70eff592056ac13de1673572fecb9
JasonYang <jason760924@gmail.com>
2016-06-11 17:57:46 +0800

    Fix test package error.

--- library/src/test/java/ui/android/dialogalchemy/ExampleUnitTest.java
+++ library/src/test/java/ui/android/dialogalchemy/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package ui.android.dialog.dialogalchemy;
+package ui.android.dialogalchemy;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
