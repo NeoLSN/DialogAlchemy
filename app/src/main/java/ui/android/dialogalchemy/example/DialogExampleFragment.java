ba4c556f68e4495e185ed4d849291541f9badc18
JasonYang <jason760924@gmail.com>
2016-06-13 17:38:17 +0800

    Color palette.

--- app/src/main/java/ui/android/dialogalchemy/example/DialogExampleFragment.java
+++ app/src/main/java/ui/android/dialogalchemy/example/DialogExampleFragment.java
@@ -3,25 +3,20 @@ package ui.android.dialogalchemy.example;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.SeekBar;
 import android.widget.Toast;
 
 import ui.android.dialogalchemy.DialogAlchemy;
 import ui.android.dialogalchemy.Material;
-import ui.android.dialogalchemy.PhilosopherStone;
 import ui.android.dialogalchemy.circle.MetalTransmutationCircle;
 import ui.android.dialogalchemy.example.circle.AlertDialogProTransmutationCircle;
 import ui.android.dialogalchemy.example.circle.MaterialDialogsTransmutationCircle;
+import ui.android.dialogalchemy.example.stone.ColorPaletteStone;
 
 /**
  * Created by JasonYang on 2016/6/7.
@@ -61,6 +56,14 @@ public class DialogExampleFragment extends Fragment {
                 createMDDialog();
             }
         });
+
+        Button colorPaletteButton = (Button) view.findViewById(R.id.color_palette_button);
+        colorPaletteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createColorPaletteDialog();
+            }
+        });
     }
 
     private void createDialog() {
@@ -80,85 +83,37 @@ public class DialogExampleFragment extends Fragment {
 
     private Material createMaterial() {
         return new Material.Builder(getActivity())
-                .setTitle("Color palette")
-                .setNegativeButton(android.R.string.cancel, null)
-                .setPhilosopherStone(new PhilosopherStone() {
-
-                    private int color = Color.CYAN;
-
-                    private ImageView colorPalette;
-                    private SeekBar aSeekBar;
-                    private SeekBar rSeekBar;
-                    private SeekBar gSeekBar;
-                    private SeekBar bSeekBar;
-
-                    private SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
-                        @Override
-                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                            updateColor();
-                        }
-
-                        @Override
-                        public void onStartTrackingTouch(SeekBar seekBar) {
-                        }
-
-                        @Override
-                        public void onStopTrackingTouch(SeekBar seekBar) {
-                            updateColor();
-                        }
-                    };
-
+                .setTitle("Title")
+                .setMessage("Message")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
-                    public int getLayoutResId() {
-                        return R.layout.dialog_color_palette;
+                    public void onClick(DialogInterface dialog, int which) {
+                        Context context = ((Dialog) dialog).getContext();
+                        Toast.makeText(context, "Button POSITIVE", Toast.LENGTH_SHORT).show();
                     }
-
-                    @NonNull
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
-                    public Material mergeMaterial(@NonNull Context context, @NonNull Material material1) {
-                        if (TextUtils.isEmpty(material1.getPositiveButtonText())) {
-                            Material.Builder builder = material1.rebuild(context);
-                            builder.setPositiveButton(android.R.string.ok, null);
-                            material1 = builder.build();
-                        }
-                        return material1;
+                    public void onClick(DialogInterface dialog, int which) {
+                        Context context = ((Dialog) dialog).getContext();
+                        Toast.makeText(context, "Button NEGATIVE", Toast.LENGTH_SHORT).show();
                     }
-
+                })
+                .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
-                    public void bindView(Dialog dialog) {
-                        colorPalette = (ImageView) dialog.findViewById(R.id.color_palette);
-                        aSeekBar = (SeekBar) dialog.findViewById(R.id.a_seek_bar);
-                        rSeekBar = (SeekBar) dialog.findViewById(R.id.r_seek_bar);
-                        gSeekBar = (SeekBar) dialog.findViewById(R.id.g_seek_bar);
-                        bSeekBar = (SeekBar) dialog.findViewById(R.id.b_seek_bar);
-
-                        colorPalette.setBackgroundColor(color);
-
-                        aSeekBar.setProgress(Color.alpha(color));
-                        rSeekBar.setProgress(Color.red(color));
-                        gSeekBar.setProgress(Color.green(color));
-                        bSeekBar.setProgress(Color.blue(color));
-
-                        aSeekBar.setOnSeekBarChangeListener(listener);
-                        rSeekBar.setOnSeekBarChangeListener(listener);
-                        gSeekBar.setOnSeekBarChangeListener(listener);
-                        bSeekBar.setOnSeekBarChangeListener(listener);
-                    }
-
-                    @Override
-                    public void onButtonClick(DialogInterface dialogInterface, int which) {
-                        if (which == Dialog.BUTTON_POSITIVE) {
-                            Context context = ((Dialog) dialogInterface).getContext();
-                            Toast.makeText(context, "color is #" + Integer.toHexString(color), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    private void updateColor() {
-                        color = Color.argb(aSeekBar.getProgress(), rSeekBar.getProgress(),
-                                gSeekBar.getProgress(), bSeekBar.getProgress());
-                        colorPalette.setBackgroundColor(color);
+                    public void onClick(DialogInterface dialog, int which) {
+                        Context context = ((Dialog) dialog).getContext();
+                        Toast.makeText(context, "Button NEUTRAL", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .build();
+    }
+
+    private void createColorPaletteDialog() {
+        Material.Builder builder = new Material.Builder(getActivity());
+        builder.setTitle("Color Palette")
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPhilosopherStone(new ColorPaletteStone());
+        DialogAlchemy.show(getFragmentManager(), builder.build());
     }
 }
